360c783fddfcde91cfc3ea745ffd5bba1385fe48
Chris Nitsas <nitsas@skroutz.gr>
2024-09-17 00:21:32 +0300

    Implement stage "Send Correlation ID"; send hardcoded correlation_id 7

--- app/server.go
+++ app/server.go
@@ -4,17 +4,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:9092")
+	listener, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	fmt.Println("Writing hardcoded response with length 4 and correlation_id 7.")
+	conn.Write([]byte{0, 0, 0, 4, 0, 0, 0, 7})
+
+	time.Sleep(5 * time.Second)
 }
