0ce657bddc9ff40ebcb7435693137db3fe6e2e77
Chris Nitsas <nitsas@skroutz.gr>
2024-10-13 14:33:21 +0300

    Implement stage "Handle APIVersions requests"

---
+++ app/api_keys.go
@@ -0,0 +1,9 @@
+package main
+
+var APIKeyVal = map[string]uint16{
+	"ApiVersions": 18,
+}
+
+var APIKeyBytes = map[string][]byte{
+	"ApiVersions": {0, 18},
+}
--- app/server.go
+++ app/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"time"
@@ -15,50 +17,96 @@ func panicIf(err error) {
 }
 
 func main() {
+	var err error
+
 	listener, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
+
 	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	buf := make([]byte, 100)
-	n, err := conn.Read(buf)
+	var buf []byte
+
+	buf = make([]byte, 4)
+	_, err = io.ReadFull(conn, buf)
 	panicIf(err)
-	if n < 12 {
-		panic(fmt.Errorf("Read too few bytes! Expected at least 12 but got %d.\n", n))
+
+	length := binary.BigEndian.Uint32(buf)
+
+	if length < 8 {
+		panic(fmt.Errorf("Expected a message of at least 8 bytes, but got a length of %d.\n", length))
 	}
 
-	// requestLength := buf[0:4]
-	requestAPIKey := int16(binary.BigEndian.Uint16(buf[4:6]))
-	requestAPIVersion := int16(binary.BigEndian.Uint16(buf[6:8]))
-	correlationId := buf[8:12]
+	buf = make([]byte, length)
+	_, err = io.ReadFull(conn, buf)
+	panicIf(err)
 
-	fmt.Printf("Got a request with API key %d and API version %d\n", requestAPIKey, requestAPIVersion)
+	requestAPIKey := binary.BigEndian.Uint16(buf[0:2])
+	requestAPIVersion := binary.BigEndian.Uint16(buf[2:4])
+	correlationIdBytes := buf[4:8]
+	correlationId := binary.BigEndian.Uint32(correlationIdBytes)
+	fmt.Printf("Got request: %#v\n", buf)
+	fmt.Printf("Got API key %d, API version %d, correlationId %d\n", requestAPIKey, requestAPIVersion, correlationId)
 
-	if requestAPIKey != 18 {
-		panic(fmt.Errorf("We only know how to respond to APIVersions requests (API key: 18)\n"))
+	respMsgSizeBytes := make([]byte, 4)
+
+	switch requestAPIKey {
+	case APIKeyVal["ApiVersions"]:
+		if requestAPIVersion < 0 || requestAPIVersion > 4 {
+			fmt.Printf("Unrecognized API version %d - expected 0, 1, 2, 3, or 4.\n", requestAPIVersion)
+			respMsg := bytes.NewBuffer(nil)
+			respMsg.Write(correlationIdBytes)
+			respMsg.Write([]byte{0, 35})
+
+			respMsgSize := respMsg.Len()
+			binary.BigEndian.PutUint32(respMsgSizeBytes, uint32(respMsgSize))
+
+			response := make([]byte, 0, len(respMsgSizeBytes)+respMsgSize)
+			response = append(response, respMsgSizeBytes...)
+			response = append(response, respMsg.Bytes()...)
+
+			_, err = conn.Write(response)
+			panicIf(err)
+
+			fmt.Printf("Wrote response with size %d, correlation_id %#v, error code 35.\n", respMsgSize, correlationIdBytes)
+		} else {
+			fmt.Printf("Got a supported API version (%d)!\n", requestAPIVersion)
+			respMsg := bytes.NewBuffer(nil)
+			respMsg.Write(correlationIdBytes)
+			respMsg.Write([]byte{0, 0}) // error code: 0
+			respMsg.Write([]byte{2})    // length of array (+1) of APIs whose versions we'll publish
+
+			// supported versions for request ApiVersions
+			respMsg.Write(APIKeyBytes["ApiVersions"])
+			respMsg.Write([]byte{0, 3})       // min version
+			respMsg.Write([]byte{0, 4})       // max version
+			respMsg.Write([]byte{0})          // _tagged_fields
+			respMsg.Write([]byte{0, 0, 0, 0}) // throttle time
+			respMsg.Write([]byte{0})          // _tagged_fields
+
+			respMsgSize := respMsg.Len()
+			// fmt.Printf("respMsgSize: %d\n", respMsgSize)
+			binary.BigEndian.PutUint32(respMsgSizeBytes, uint32(respMsgSize))
+
+			response := make([]byte, 0, len(respMsgSizeBytes)+respMsgSize)
+			response = append(response, respMsgSizeBytes...)
+			response = append(response, respMsg.Bytes()...)
+
+			_, err = conn.Write(response)
+			panicIf(err)
+
+			fmt.Printf("Wrote response %#v\n", response)
+		}
+	default:
+		panic(fmt.Errorf("We only know how to respond to APIVersions requests (API key: %d)\n", APIKeyVal["ApiVersions"]))
 	}
 
-	if requestAPIVersion < 0 || requestAPIVersion > 4 {
-		fmt.Printf("Unrecognized API version %d - expected 0, 1, 2, 3, or 4.\n", requestAPIVersion)
-		conn.Write([]byte{0, 0, 0, 6})
-		conn.Write(correlationId)
-
-		responseCodeUnsupportedVersion := make([]byte, 2)
-		binary.BigEndian.PutUint16(responseCodeUnsupportedVersion, uint16(35))
-		conn.Write(responseCodeUnsupportedVersion)
-
-		fmt.Printf("Wrote response with length 6, correlation_id %#v, error code 35.\n", correlationId)
-	} else {
-		fmt.Printf("Got a supported API version. Responding with the correlation id.\n")
-		conn.Write([]byte{0, 0, 0, 4})
-		conn.Write(correlationId)
-	}
-
-	time.Sleep(5 * time.Second)
+	time.Sleep(3 * time.Second)
 }
